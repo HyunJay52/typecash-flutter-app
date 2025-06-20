174203e5a1aed6b32f30ccffe233bcb1a1dba48b
Kim HyunJung <ctrlpjay@gmail.com>
2025-06-20 17:05:00 +0900

    feat: base api outline, base environment setting

--- lib/config/environment.dart
+++ lib/config/environment.dart
@@ -1,27 +1,91 @@
-class Environment {
-  // static const String apiUrl = 'https://api.example.com';
+import 'package:flutter/foundation.dart';
+import 'package:typecash_flutter_app/config/app_settings.dart';
 
-  static late final String apiUrl;
-  
-  Environment(String environment, String server) {
-    if (environment == 'production') {
-      apiUrl = 'https://api.example.com';
-    } else if (environment == 'staging') {
-      apiUrl = 'https://staging-api.example.com';
-    } else if (environment == 'development') {
-      apiUrl = 'http://localhost:3000';
-    } else {
-      throw Exception('Invalid environment: $environment');
-    }
+enum EnvironmentType { production, staging, development, local }
 
-    if (server == 'production') {
-      apiUrl = 'https://api.example.com';
-    } else if (server == 'staging') {
-      apiUrl = 'https://staging-api.example.com';
-    } else if (server == 'development') {
-      apiUrl = 'http://localhost:3000';
-    } else {
-      throw Exception('Invalid server: $server');
+enum ServerType { production, staging, development, local }
+
+class EnvironmentConfig {
+  final EnvironmentType environmentType;
+  final ServerType serverType;
+
+  EnvironmentConfig({required this.environmentType, required this.serverType});
+
+  /// flavor 문자열로부터 환경 설정 생성
+  static EnvironmentConfig fromFlavor(String flavor) {
+    switch (flavor) {
+      case 'prod':
+        return EnvironmentConfig(
+          environmentType: EnvironmentType.production,
+          serverType: ServerType.production,
+        );
+      case 'dev':
+        return EnvironmentConfig(
+          environmentType: EnvironmentType.development,
+          serverType: ServerType.development,
+        );
+      case 'staging':
+        return EnvironmentConfig(
+          environmentType: EnvironmentType.staging,
+          serverType: ServerType.staging,
+        );
+      default:
+        return EnvironmentConfig(
+          environmentType: EnvironmentType.local,
+          serverType: ServerType.local,
+        );
     }
   }
+}
+
+class Environment {
+  static final Environment _instance = Environment._internal();
+  factory Environment() => _instance;
+
+  static late EnvironmentType environmentType;
+  static late ServerType serverType;
+
+  static late final String apiUrl;
+
+  bool _isInitialized = false;
+
+  Environment._internal();
+
+  void initialize({
+    required EnvironmentType environment,
+    required ServerType server,
+  }) {
+    if (_isInitialized) return;
+
+    environmentType = environment;
+    serverType = server;
+
+    // final appSettings = _getAppSettings
+
+    _isInitialized = true;
+
+     debugPrint(
+      'Environment initialized: $environmentType, Server: $serverType',
+    );
+  }
+
+  AppSettings getAppSettings(ServerType type) {
+    switch (type) {
+      case ServerType.production:
+        return ProductionAppSettings();
+      case ServerType.staging:
+        return StagingAppSettings();
+      case ServerType.development:
+        return DevelopmentAppSettings();
+      case ServerType.local:
+        return LocalAppSettings();
+    }
+  }
+
+  static bool get isProd => environmentType == EnvironmentType.production;
+  static bool get isStaging => environmentType == EnvironmentType.staging;
+  static bool get isDevelopment =>
+      environmentType == EnvironmentType.development;
+  static bool get isLocal => environmentType == EnvironmentType.local;
+  
 }
